ce74b1042bfd135c8e9c48c0a255360fba88c5f2
angelicarm3 <angie00069@gmail.com>
2022-05-03 22:56:32 -0500

    0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixing Apache's 500 error
+exec { 'wordpress-webpage':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+    path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
