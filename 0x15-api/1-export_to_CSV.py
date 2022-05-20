9c48c651f08f0987d3b82440b89603283d5f8e0a
angelicarm3 <angie00069@gmail.com>
2022-05-19 22:42:35 -0500

    Revert "doc?"
    This reverts commit 3ad286b672a7546baa6c947b4ead3a2bd2b1706e.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
-Using task 0 extend script to xport data in the CSV format
-"""
+"""Using task 0 extend script to xport data in the CSV format"""
 
-import requests
+
+if __name__ == '__main__':
+    import requests
     import sys
     import csv
-    
-if __name__ == '__main__':
-    """"""
+
     employee_id = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(employee_id)).json()
