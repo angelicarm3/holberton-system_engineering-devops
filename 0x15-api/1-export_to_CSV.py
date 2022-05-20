3ad286b672a7546baa6c947b4ead3a2bd2b1706e
angelicarm3 <angie00069@gmail.com>
2022-05-19 22:40:09 -0500

    doc?

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Using task 0 extend script to xport data in the CSV format"""
+"""
+Using task 0 extend script to xport data in the CSV format
+"""
 
-
-if __name__ == '__main__':
-    import requests
+import requests
     import sys
     import csv
-
+    
+if __name__ == '__main__':
+    """"""
     employee_id = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(employee_id)).json()
